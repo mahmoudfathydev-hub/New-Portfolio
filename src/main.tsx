17430485f87717fcf8bf7482cfdbfd342550f416
Mahmoud Fathy <mahmoudfathy.dev@gmail.com>
2026-01-12 12:50:44 +0200

    fix error

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/portfolio3d/">
+    <BrowserRouter basename="/New-Portfolio/">
       <App />
     </BrowserRouter>
   </StrictMode>,

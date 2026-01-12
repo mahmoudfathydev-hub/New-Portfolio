e945f9f91b3661b7b153649dc546254667f18c85
Mahmoud Fathy <mahmoudfathy.dev@gmail.com>
2026-01-12 16:53:07 +0200

    5

--- src/components/Home/WhyMe.tsx
+++ src/components/Home/WhyMe.tsx
@@ -86,7 +86,6 @@ const WhyMe = () => {
                     <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%]
                                     shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10" />
 
-                    <FloatingShapes />
 
                     {/* Swiper */}
                     <Swiper

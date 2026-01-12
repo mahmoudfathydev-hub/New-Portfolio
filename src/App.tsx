24a3c1669c8c0f2dbcb544355a92ff8c6a44a2a8
Mahmoud Fathy <mahmoudfathy.dev@gmail.com>
2026-01-12 14:07:58 +0200

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-const gradientBackground = "/images/gradient.png";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
-import About from './pages/About';
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Global/Navbar';
-import Footer from './components/Global/Footer';
-import Project from './pages/Project';
-import ProjectInfo from './pages/ProjectInfo';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
+import About from './pages/About'
+import Project from './pages/Project'
+import ProjectInfo from './pages/ProjectInfo'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import Navbar from './components/Global/Navbar'
+import Footer from './components/Global/Footer'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
+
+const gradientBackground = "/images/gradient.png";
+
 export default function App() {
   useEffect(() => {
     AOS.init({
@@ -18,10 +20,15 @@ export default function App() {
       once: true,
     });
   }, [])
+
   return (
-    <>
+    <Router>
       <main>
-        <img className='absolute top-0 right-0 opacity-60 -z-10' src={gradientBackground} alt="Gradient" />
+        <img
+          className='absolute top-0 right-0 opacity-60 -z-10'
+          src={gradientBackground}
+          alt="Gradient"
+        />
         <div className='h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10'></div>
         <Navbar />
         <Routes>
@@ -34,6 +41,6 @@ export default function App() {
         </Routes>
         <Footer />
       </main>
-    </>
+    </Router>
   )
 }

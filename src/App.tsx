de8344b9a56c7a8b16b1893506ec80db0d532bf2
Mahmoud Fathy <mahmoudfathy.dev@gmail.com>
2026-01-12 17:11:06 +0200

    730

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import About from './pages/About'
 import Project from './pages/Project'
